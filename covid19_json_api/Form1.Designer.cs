9161c251b3a32486b665f21da739b9971f96630a
ONUR <onurkalyoncu18@gmail.com>
2022-07-19 12:47:10 +0300

    Add project files.

---
+++ covid19_json_api/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace covid19_json_api
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "TARİH"}, -1, System.Drawing.Color.Empty, System.Drawing.Color.Empty, new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point));
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            "TEST SAYISI"}, -1, System.Drawing.Color.Empty, System.Drawing.Color.Empty, new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point));
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
+            "VAKA"}, -1, System.Drawing.Color.Empty, System.Drawing.Color.Empty, new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point));
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem(new string[] {
+            "İYİLEŞEN"}, -1, System.Drawing.Color.Empty, System.Drawing.Color.Empty, new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point));
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Vefat"}, -1, System.Drawing.Color.Empty, System.Drawing.Color.Empty, new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point));
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.tarih = new System.Windows.Forms.ColumnHeader();
+            this.test = new System.Windows.Forms.ColumnHeader();
+            this.vaka = new System.Windows.Forms.ColumnHeader();
+            this.iyilesen = new System.Windows.Forms.ColumnHeader();
+            this.vefat = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(284, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(613, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GÜNLÜK COVİD-19 VERİLERİ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.tarih,
+            this.test,
+            this.vaka,
+            this.iyilesen,
+            this.vefat});
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4,
+            listViewItem5});
+            this.listView1.Location = new System.Drawing.Point(12, 69);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1347, 493);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Tile;
+            // 
+            // test
+            // 
+            this.test.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // vaka
+            // 
+            this.vaka.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // iyilesen
+            // 
+            this.iyilesen.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // vefat
+            // 
+            this.vefat.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1381, 574);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader tarih;
+        private System.Windows.Forms.ColumnHeader test;
+        private System.Windows.Forms.ColumnHeader vaka;
+        private System.Windows.Forms.ColumnHeader iyilesen;
+        private System.Windows.Forms.ColumnHeader vefat;
+    }
+}
+
---
+++ covid19_json_api/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace covid19_json_api
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String[] CoranaVerileri, CoranaGunlukVeriBilgisi;
+
+            using (WebClient wc = new WebClient())
+            {
+                var url = wc.DownloadString("https://raw.githubusercontent.com/ozanerturk/covid19-turkey-api/master/dataset/timeline.json");
+
+                CoranaVerileri = url.ToString().Split('{');
+            }
+
+            for (int i = 1; i <=10; i++)
+            {
+                CoranaGunlukVeriBilgisi = CoranaVerileri[CoranaVerileri.Length-i].Split('"');
+
+                if (i==1)
+                {
+                    label1.Text = CoranaGunlukVeriBilgisi[3] + "COVİD HASTA TABLOSU";
+                }
+
+                tarih.ListView.Items.Add(CoranaGunlukVeriBilgisi[3]);
+                test.ListView.Items.Add(CoranaGunlukVeriBilgisi[31]);
+                vaka.ListView.Items.Add(CoranaGunlukVeriBilgisi[35]);
+                iyilesen.ListView.Items.Add(CoranaGunlukVeriBilgisi[51]);
+                vefat.ListView.Items.Add(CoranaGunlukVeriBilgisi[47]);
+            }
+
+            
+        }
+    }
+}
